a93b2bac1b8ba9b2c7f4fa8dd444bff2fbdf783d
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-25 13:37:42 -0400

    modify

--- polls/serializers.py
+++ polls/serializers.py
@@ -12,24 +12,28 @@ class VoteSerializer(serializers.ModelSerializer):
 class PollSubjectSerializer(serializers.ModelSerializer):
 
     votes = VoteSerializer(many=True)
-    total_votes = serializers.SerializerMethodField()
+
+    percentage_of_votes = serializers.SerializerMethodField()
 
     class Meta:
         model = PollSubject
-        fields = ('id', 'name', 'votes', 'total_votes')
+        fields = ('id', 'name', 'votes', 'percentage_of_votes')
 
-    def get_total_votes(self, subject):
-        return subject.votes.count()
+    def get_percentage_of_votes(self, subject):
+        return subject.votes.count()*100/subject.poll.votes.count()
 
 
 class PollSerializer(serializers.ModelSerializer):
 
     subjects = PollSubjectSerializer(many=True)
+
     user_has_voted = serializers.SerializerMethodField()
+
+    total_votes = serializers.SerializerMethodField()
 
     class Meta:
         model = Poll
-        fields = ('id', 'question', 'subjects', 'user_has_voted')
+        fields = ('id','thread', 'question', 'subjects', 'user_has_voted', 'total_votes')
 
     def get_user_has_voted(self, poll):
         has_voted = False
@@ -39,11 +43,17 @@ class PollSerializer(serializers.ModelSerializer):
         if not request:
             return False
 
+        if not request.user.is_authenticated():  # don't show vote ticks if they are not logged in!
+            return True
+
         vote = poll.votes.filter(user_id=request.user.id).first()
         if vote:
             has_voted = True
 
         return has_voted
+
+    def get_total_votes(self, poll):
+        return poll.votes.count()
 
 
 class ThreadSerializer(serializers.ModelSerializer):
